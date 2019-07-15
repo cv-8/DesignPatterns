420865dae18f9501c9972f5835e5fe987c6f9420
james.cummings <james.cummings@northumberland.gov.uk>
2019-07-15 11:24:41 +0100

    JCTest commit

---
+++ JCTemp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace JCTemp{
+
+public class Demo
+  {
+      private static int Printer(int j)
+      {
+          for (var i = j; i > 0; i = Printer(i - 1))
+          {
+              WriteLine(i);
+          }
+          return j;
+      }
+      
+    static void Main(string[] args)
+    {
+        Printer(2);
+    }
+  }
+
+
+
+
+}
